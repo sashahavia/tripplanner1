6efaf2cc8234872ef928c3327e44752c9d8cdfa5
Alexandra Havia <sasha.havia@gmail.com>
2018-06-19 16:09:18 -0400

    Implemented mapbox API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Index.js");
+// console.log("Index.js");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FzaGEtaGF2aWEiLCJhIjoiY2pkdGN3MW1hMHpzNDJ4bnk4NzBnOXVzeSJ9.9a0w0WPYRY4qJC0dDSUnKg';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
