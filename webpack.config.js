12b59ae5735c51d3cd545352ded1b1b769663f05
Alexandra Havia <sasha.havia@gmail.com>
2018-06-19 15:54:11 -0400

    Installed Webpack and mapbox

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "development", // "production" | "development" | "none"
+  // Chosen mode tells webpack to use its built-in optimizations accordingly.
+  entry: "./src/index.js", // string | object | array
+  // defaults to ./src
+  // Here the application starts executing
+  // and webpack starts bundling
+  output: {
+    // options related to how webpack emits results
+    path: __dirname+  "/public", // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+    filename: "bundle.js", // string
+  }
+}
